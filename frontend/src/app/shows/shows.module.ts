3061aca63126f71cfc945cab9647d67e69cf1eec
Alberto Fuentes <afuentes@ES-MBP-AFUENTES.local>
2020-01-17 11:11:21 +0100

    shows module added

---
+++ frontend/src/app/shows/shows.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShowsRoutingModule } from './shows-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ShowsRoutingModule
+  ]
+})
+export class ShowsModule { }
