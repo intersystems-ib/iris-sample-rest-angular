cb8868111d5c372f70baec9b34b6ee476c1ca8b6
Alberto Fuentes <afuentes@ES-MBP-AFUENTES.local>
2020-02-04 09:41:27 +0100

    restforms2 endpoint updated

--- frontend/src/app/shows/services/shows.service.ts
+++ frontend/src/app/shows/services/shows.service.ts
@@ -14,7 +14,7 @@ import { AlertService } from 'src/app/shared/services/alert.service';
 export class ShowsService {
   
   /** Base URL */
-  private urlBase = 'http://localhost:52773/myapp/api/form';
+  private urlBase = 'http://localhost:52773/myapp/api';
   
   /** Options used in request */
   private options = { withCredentials: true };
@@ -40,7 +40,7 @@ export class ShowsService {
     escapedFilter = escapedFilter.replace(new RegExp('\\+'), '');
     
     return this.http.get<QueryResult<Show>>(
-      this.urlBase + `/objects/App.Data.Show/find?size=${pageSize}&page=${pageIndex}&collation=UPPER&filter=${escapedFilter}`,
+      this.urlBase + `/rf2/form/objects/App.Data.Show/find?size=${pageSize}&page=${pageIndex}&collation=UPPER&filter=${escapedFilter}`,
       this.options
     ).pipe(
       //tap(data => console.log(data))
@@ -57,7 +57,7 @@ export class ShowsService {
   findLatestShows(): Observable<QueryResult<Show>> {
     return this.http.get<QueryResult<Show>>(
       this.urlBase + 
-        '/objects/App.Data.Show/find?size=10&page=1&collation=UPPER&'+
+        '/rf2/form/objects/App.Data.Show/find?size=10&page=1&collation=UPPER&'+
         'filter=showid%20in%2070136135~80057281~80114855~80025678~80211627~80175798',
       this.options
     ).pipe(
@@ -75,7 +75,7 @@ export class ShowsService {
    */
   findShowById(id: number): Observable<Show> {
     return this.http.get<Show>(
-      this.urlBase + `/object/App.Data.Show/${id}`,
+      this.urlBase + `/rf2/form/object/App.Data.Show/${id}`,
       this.options
     ).pipe(
       //tap(data => console.log(data)),
@@ -92,7 +92,7 @@ export class ShowsService {
    */
   findCastByShow(id: number): Observable<QueryResult<Cast>> {
     return this.http.get<QueryResult<Cast>>(
-      this.urlBase + `/objects/App.Data.Cast/find?filter=show+eq+${id}`,
+      this.urlBase + `/rf2/form/objects/App.Data.Cast/find?filter=show+eq+${id}`,
       this.options
     ).pipe(
       //tap(data => console.log(data)),
@@ -110,7 +110,7 @@ export class ShowsService {
    */
   saveShow(id: number, show: Show) {
     return this.http.put(
-      this.urlBase + `/object/App.Data.Show/${id}`,
+      this.urlBase + `/rf2/form/object/App.Data.Show/${id}`,
       show,
       this.options
     ).pipe(
@@ -128,7 +128,7 @@ export class ShowsService {
    */
   saveCast(id: number, cast: Cast) {
     return this.http.put(
-      this.urlBase + `/object/App.Data.Cast/${id}`,
+      this.urlBase + `/rf2/form/object/App.Data.Cast/${id}`,
       cast,
       this.options
     ).pipe(
